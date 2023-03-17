f86382b02eba91eaf88b61ef8553cfb4214dfc0c
macvincent <macvincentaghaoko@gmail.com>
2023-03-17 07:17:09 -0700

    experiment with four rasterization methods. None works

---
+++ mesh_sample_utils_o3d.py
@@ -0,0 +1,56 @@
+import torch
+import matplotlib.pyplot as plt
+import copy
+import open3d as o3d
+import numpy as np
+import imageio
+from tqdm import tqdm
+
+device=torch.device("cuda:0")
+
+def rotate_mesh(mesh, rotate_angle=-np.pi):
+    rotated_mesh = copy.deepcopy(mesh)
+    rotate_to_torch_axis = rotated_mesh.get_rotation_matrix_from_xyz((0, 0, rotate_angle))
+    rotated_mesh.rotate(rotate_to_torch_axis, center=(0, 0, 0))
+    # o3d.visualization.draw_geometries([rotated_mesh])
+
+    return rotated_mesh
+
+def get_rotated_frame(mesh, rotate_angle):
+    rotated_mesh = copy.deepcopy(mesh)
+    rotate_to_torch_axis = rotated_mesh.get_rotation_matrix_from_xyz((rotate_angle, rotate_angle, rotate_angle))
+    rotated_mesh.rotate(rotate_to_torch_axis, center=(0, 0, 0))
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(visible=False)
+    vis.add_geometry(rotated_mesh)
+    vis.update_geometry(rotated_mesh)
+    vis.poll_events()
+    vis.update_renderer()
+
+    # Run the visualizer
+    color_image = vis.capture_screen_float_buffer(do_render=True)
+    vis.destroy_window()
+    return np.asarray(color_image)
+
+foreground_visibility_mesh = o3d.io.read_triangle_mesh('foreground_visibility_mesh.ply')
+foreground_visibility_mesh = rotate_mesh(foreground_visibility_mesh)
+
+background_mesh = o3d.io.read_triangle_mesh('background_mesh.ply')
+background_mesh = rotate_mesh(background_mesh)
+
+foreground_mesh = o3d.io.read_triangle_mesh('foreground_mesh.ply')
+foreground_mesh = rotate_mesh(foreground_mesh)
+
+initial_angle = -10
+frames = []
+for i in tqdm(range(20)):
+    rotate_angle = (initial_angle + i) * np.pi / 180
+    background_image = get_rotated_frame(background_mesh, rotate_angle ) * 255
+    foreground_image = get_rotated_frame(foreground_mesh, rotate_angle) * 255
+    foreground_visibility_map = get_rotated_frame(foreground_visibility_mesh, rotate_angle)
+
+    novel_view = foreground_visibility_map*foreground_image + (1-foreground_visibility_map)*background_image
+    novel_view = np.clip(np.asarray(novel_view), 0, 255).astype(np.uint8)
+
+    frames.append(novel_view)
+imageio.mimsave('./movie_o3d.gif', frames)
---
+++ mesh_sample_utils_vispy.py
@@ -0,0 +1,109 @@
+import copy
+import open3d as o3d
+import numpy as np
+import imageio
+from tqdm import tqdm
+from vispy import app, gloo, visuals, scene
+from vispy.scene import visuals
+from vispy.visuals.filters import Alpha
+
+class Canvas_view():
+    def __init__(self,
+                 fov,
+                 verts,
+                 faces,
+                 colors,
+                 canvas_size,
+                 factor=1,
+                 bgcolor='gray',
+                 proj='perspective',
+                 ):
+        self.canvas = scene.SceneCanvas(bgcolor=bgcolor, size=(canvas_size*factor, canvas_size*factor))
+        self.view = self.canvas.central_widget.add_view()
+        self.view.camera = proj
+        self.view.camera.fov = fov
+        self.mesh = visuals.Mesh(shading=None)
+        self.mesh.attach(Alpha(1.0))
+        self.view.add(self.mesh)
+        self.tr = self.view.camera.transform
+        self.mesh.set_data(vertices=verts, faces=faces, vertex_colors=colors[:, :3])
+        self.translate([0,0,0])
+        self.rotate(axis=[1,0,0], angle=180)
+        self.view_changed()
+
+    def translate(self, trans=[0,0,0]):
+        self.tr.translate(trans)
+
+    def rotate(self, axis=[1,0,0], angle=0):
+        self.tr.rotate(axis=axis, angle=angle)
+
+    def view_changed(self):
+        self.view.camera.view_changed()
+
+    def render(self):
+        return self.canvas.render()
+
+    def reinit_mesh(self, verts, faces, colors):
+        self.mesh.set_data(vertices=verts, faces=faces, vertex_colors=colors[:, :3])
+
+    def reinit_camera(self, fov):
+        self.view.camera.fov = fov
+        self.view.camera.view_changed()
+
+
+def rotate_mesh(mesh, rotate_angle=np.pi):
+    rotated_mesh = copy.deepcopy(mesh)
+    rotate_to_torch_axis = rotated_mesh.get_rotation_matrix_from_xyz((0, 0, rotate_angle))
+    rotated_mesh.rotate(rotate_to_torch_axis, center=(0, 0, 0))
+
+    return rotated_mesh
+
+
+def get_vispy_canvas(open_3d_mesh):
+    colors = np.array(open_3d_mesh.vertex_colors)
+    verts = np.array(open_3d_mesh.vertices)
+    faces = np.array(open_3d_mesh.triangles)
+    normals = np.array(open_3d_mesh.vertex_normals)
+
+    normal_canvas = Canvas_view(45,
+                                verts,
+                                faces,
+                                colors,
+                                canvas_size=512,
+                                factor=1,
+                                bgcolor='gray',
+                                proj='arcball')
+
+    return normal_canvas
+
+def get_rotated_frame(mesh, rotate_angle):
+    rotated_mesh = copy.deepcopy(mesh)
+    rotate_to_torch_axis = rotated_mesh.get_rotation_matrix_from_xyz((0, 0, rotate_angle))
+    rotated_mesh.rotate(rotate_to_torch_axis, center=(0, 0, 0))
+
+    canvas = get_vispy_canvas(rotated_mesh)
+
+    return canvas.render()[:, :, :3]
+
+foreground_visibility_mesh = o3d.io.read_triangle_mesh('foreground_visibility_mesh.ply')
+foreground_visibility_mesh = rotate_mesh(foreground_visibility_mesh)
+
+background_mesh = o3d.io.read_triangle_mesh('background_mesh.ply')
+background_mesh = rotate_mesh(background_mesh)
+
+foreground_mesh = o3d.io.read_triangle_mesh('foreground_mesh.ply')
+foreground_mesh = rotate_mesh(foreground_mesh)
+
+initial_angle = -10
+frames = []
+for i in tqdm(range(20)):
+    rotate_angle = (initial_angle + i) * np.pi / 180
+    background_image = get_rotated_frame(background_mesh, rotate_angle) 
+    foreground_image = get_rotated_frame(foreground_mesh, rotate_angle)
+    foreground_visibility_map = get_rotated_frame(foreground_visibility_mesh, rotate_angle)
+    alpha = foreground_visibility_map / foreground_visibility_map.max()
+    novel_view = alpha*foreground_image + (1-alpha)*background_image
+    novel_view = np.clip(np.asarray(novel_view), 0, 255).astype(np.uint8)
+
+    frames.append(novel_view)
+imageio.mimsave('./movie_vispy.gif', frames)
